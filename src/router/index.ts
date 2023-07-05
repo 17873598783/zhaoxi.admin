36a5db38c6ecbee6bb935ae2279695fecb9f6405
17873598783 <978233724@qq.com>
2023-07-05 17:24:57 +0800

    菜单模块静态页

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../view/index/LoginPage.vue'
 import RootPage from '../view/index/RootPage.vue'
 import DeskTop from '../view/index/DeskTop.vue'
+import PersonCenter from '../view/index/PersonCenter.vue'
+import MenuManager from '../view/admin/menu/MenuManager.vue'
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
             path: "/", component: RootPage, children: [
-                {name:"工作台",path: "/desktop", component: DeskTop}
+                {name:"工作台",path: "/desktop", component: DeskTop},
+                {name:"个人信息",path: "/person", component: PersonCenter},
+                {name:"菜单管理",path: "/menumanager", component: MenuManager}
             ]
         },
         { path: "/login", component: LoginPage }
---
+++ src/view/index/PersonCenter.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-row>
+        <el-col :span="6">
+            <el-form :model="form" label-width="120px">
+                <el-form-item label="昵称">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input v-model="form.password"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">提交</el-button>
+                    <el-button>取消</el-button>
+                </el-form-item>
+            </el-form>
+        </el-col>
+    </el-row>
+</template>
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import { useStore } from 'vuex'
+const form = reactive({
+    name: useStore().state.NickName,
+    password: ''
+})
+const onSubmit = async () => {
+    console.log('submit!');
+}
+</script>
